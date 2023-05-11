49126cff71d20bdf85fdedf2fc68cf451e4649ef
Luara Lima <luaralima205@gmail.com>
2023-05-11 18:17:28 -0300

    models added

---
+++ backend/src/models/tasksModel.js
@@ -0,0 +1,47 @@
+const connection = require("./connection");
+
+const getAll = async () => {
+    const [tasks] = await connection.execute("SELECT * FROM tasks");
+    return tasks;
+};
+
+const addTask = async (task) => {
+    const { title } = task;
+
+    const dateUTC = new Date(Date.now()).toUTCString();
+
+    const query = "INSERT INTO tasks(title, status, created_at) VALUES (?,?,?)";
+
+    const [addedTask] = await connection.execute(query, [
+        title,
+        "pendente",
+        dateUTC,
+    ]);
+
+    return { insertId: addedTask.insertId };
+};
+
+const deleteTask = async (id) => {
+    const removedTask = await connection.execute(
+        "DELETE FROM tasks WHERE id = ?",
+        [id]
+    );
+    return removedTask;
+};
+
+const updateTask = async (id, task) => {
+    const { title, status } = task;
+
+    const query = "UPDATE tasks SET title = ?, status = ? WHERE id = ?";
+    
+    const updatedTask = await connection.execute(query, [ title, status, id ]);
+    
+    return updatedTask;
+};
+
+module.exports = {
+    getAll,
+    addTask,
+    deleteTask,
+    updateTask,
+};
