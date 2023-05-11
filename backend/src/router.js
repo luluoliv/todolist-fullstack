31b4ede678fcf06beb8067017c33896cf3734ea2
Luara Lima <luaralima205@gmail.com>
2023-05-11 18:18:41 -0300

    routers added

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,7 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 const tasksController = require("./controllers/tasksController");
+const tasksMiddleware = require("./middlewares/tasksMiddleware");
 
 router.get("/tasks", tasksController.getAll);
+router.post("/tasks", tasksMiddleware.validateTitle, tasksController.addTask);
+router.delete("/tasks/:id", tasksController.deleteTask);
+router.put(
+    "/tasks/:id",
+    tasksMiddleware.validateTitle,
+    tasksMiddleware.validateStatus,
+    tasksController.updateTask
+);
 
 module.exports = router;
