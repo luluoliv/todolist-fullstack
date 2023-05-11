e0c6971f6868e75c60b3062384aa24867df52b19
Luara Lima <luaralima205@gmail.com>
2023-05-11 18:19:04 -0300

    controllers added

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,10 +1,32 @@
-const taskModel = require("../models/taskModel");
+const tasksModel = require("../models/tasksModel");
 
 const getAll = async (_request, response) => {
-    const tasks = await tasksModel.getAll;
+    const tasks = await tasksModel.getAll();
     return response.status(200).json(tasks);
+};
+
+const addTask = async (request, response) => {
+    const addedTask = await tasksModel.addTask(request.body);
+    return response.status(201).json(addedTask);
+};
+
+const deleteTask = async (request, response) => {
+    const { id } = request.params;
+
+    await tasksModel.deleteTask(id);
+    return response.status(204).json();
+};
+
+const updateTask = async (request, response) => {
+    const { id } = request.params;
+
+    await tasksModel.updateTask(id, request.body);
+    return response.status(204).json();
 };
 
 module.exports = {
     getAll,
+    addTask,
+    deleteTask,
+    updateTask
 };
